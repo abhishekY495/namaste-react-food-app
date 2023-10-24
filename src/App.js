ade5695e2d0d13f79b10f4c4fe616dc11464225f
Abhishek <abhishekY495.sec@gmail.com>
2023-10-24 17:18:49 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>Food App</div>;
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
