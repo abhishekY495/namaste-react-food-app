57864bbf69c1c655180c37a1aadcff97e4d660aa
Abhishek <abhishekY495.sec@gmail.com>
2023-10-26 15:31:40 +0530

    Feat: fetching data from api

--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import restaurantsData from "../utils/mockData";
+import { API_URL } from "../utils/constants";
 
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const RestaurantsContainer = () => {
-  const [restaurantList, setRestaurantList] = useState(restaurantsData);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
   const [topRated, setTopRated] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const topRatedBtnHandler = () => {
     setTopRated(!topRated);
     if (!topRated) {
-      const filteredRestaurantList = restaurantsData.filter(
+      const filteredRestaurantList = restaurantList.filter(
         (restaurant) => restaurant.info.avgRating > 4
       );
-      setRestaurantList(filteredRestaurantList);
+      setFilteredRestaurantList(filteredRestaurantList);
     } else {
-      setRestaurantList(restaurantsData);
+      setFilteredRestaurantList(restaurantList);
     }
   };
+
+  const fetchRestaurantsData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setFilteredRestaurantList(data);
+      setRestaurantList(data);
+    } catch (error) {
+      console.log(error);
+      setFilteredRestaurantList(restaurantsData);
+      setRestaurantList(restaurantsData);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurantsData();
+  }, []);
 
   return (
     <>
@@ -32,8 +56,23 @@ const RestaurantsContainer = () => {
       </div>
       {/*  */}
       <div id="restaurant-container">
-        {restaurantList.map((restaurant, index) => {
-          return <RestaurantCard key={index} restaurantData={restaurant} />;
+        {loading && (
+          <>
+            <Shimmer />
+            <Shimmer />
+            <Shimmer />
+            <Shimmer />
+            <Shimmer />
+            <Shimmer />
+          </>
+        )}
+        {filteredRestaurantList.map((restaurant) => {
+          return (
+            <RestaurantCard
+              key={restaurant?.info?.id}
+              restaurantData={restaurant}
+            />
+          );
         })}
       </div>
     </>
