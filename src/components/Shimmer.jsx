6553662b94246c7ad3db71d1fe10940e951510a1
Abhishek <abhishekY495.sec@gmail.com>
2023-10-26 15:29:15 +0530

    Created Shimmer.jsx

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="restaurant-details-shimmer-card">
+      <p className="restaurant-image"></p>
+      <p className="restaurant-name"></p>
+      <p className="rating-and-sla"></p>
+      <p className="cuisines-list"></p>
+      <p className="locality"></p>
+    </div>
+  );
+};
+
+export default Shimmer;
