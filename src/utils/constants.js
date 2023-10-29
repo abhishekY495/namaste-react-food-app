3abc1b1614cb20143aedbc75a0d40f6847d0e998
Abhishek <abhishekY495.sec@gmail.com>
2023-10-29 17:37:26 +0530

    Added MENU_ITEM_IMAGE_CDN url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,10 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const API_URL = "https://restaurants-api.abhisheky495.repl.co/restaurants";
+export const API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.829469&lng=86.515993&catalog_qa=undefined&submitAction=ENTER&restaurantId=";
+
+export const MENU_ITEM_IMAGE_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
