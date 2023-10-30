61370ac1893b74781ec4ad18bda9e5b691ddfa53
Abhishek <abhishekY495.sec@gmail.com>
2023-10-30 15:53:11 +0530

    Created OfflineCard.jsx

---
+++ src/components/OfflineCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const OfflineCard = () => {
+  return (
+    <div className="offline-card">
+      <p>
+        You are <b>Offline</b>
+      </p>
+      <p>Check your Internet connection</p>
+    </div>
+  );
+};
+
+export default OfflineCard;
