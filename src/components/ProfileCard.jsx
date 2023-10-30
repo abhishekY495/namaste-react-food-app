880f03b3f77cfd0f5b3e75e1a69270baa071874a
Abhishek <abhishekY495.sec@gmail.com>
2023-10-30 15:53:41 +0530

    Refactor

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,6 @@ import React from "react";
 class ProfileCard extends React.Component {
   render() {
     const { profileData } = this.props;
-    console.log(profileData);
     return (
       <div className="profile-card">
         <img src={profileData?.avatar_url} alt={profileData.name} />
